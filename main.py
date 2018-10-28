14c8eb78a4ed0ef1fea93a938dab1ffcdb991512
BiffWebster <biffwebster@gmail.com>
2018-10-28 03:02:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,22 +11,22 @@ app.secret_key = '48957w9875kjsdhfkahsdkj'
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
+    b_title = db.Column(db.String(50))
     blog = db.Column(db.String(600))
 
 
-    def __init__(self, name, blog):
-        self.name = name
+    def __init__(self, b_title, blog):
+        self.b_title= b_title
         self.blog = blog
 
-def blog_name_validate(name):
-    if name == "":
+def validate_title(b_title):
+    if b_title == "":
         return "enter a title"
     else:
         return ""
 
-def blog_body_validate(content):
-    if content == "":
+def validate_blog(blog):
+    if blog == "":
         return "please enter a paragraph"
     else:
         return ""
@@ -38,10 +38,10 @@ def index():
 
     id = request.args.get('id')
     if id != None:
-        data=Blog.query.filter_by(id=id).all()
-        return render_template('blog.html', title=data[0].name, blog=data[0], one_post=True)
+        info=Blog.query.filter_by(id=id).all()
+        return render_template('blog.html', title=info[0].name, blog=info[0], one_post=True)
     else:
-        data = Blog.query.all()
+        info = Blog.query.all()
         return render_template('blog.html', title="Build A Blog", blogs=data, one_post=False)
 
 
@@ -49,9 +49,9 @@ def index():
 @app.route('/newpost', methods=['GET', 'POST'])
 def new_blog():
     if request.method == 'POST':
-        blog_name = request.form.get('name')
-        blog_body = request.form.get('content')
-        if blog_name_validate(blog_name) or blog_body_validate(blog_body) != "":
+        b_title = request.form.get('b_title')
+        blog = request.form.get('blog')
+        if validate_title(b_title) or validate_blog(blog) != "":
             return render_template('newpost.html', title="Enter new blog", title_error=blog_name_validate(blog_name), content_error=blog_body_validate(blog_body), old_name=blog_name, old_entry=blog_body)
         else:
             new_blog = Blog(blog_name, blog_body)
